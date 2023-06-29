0782b9c106fabafd62f9ec9a24fe4788581dda4d
yoo-youngeun <gsm04380@naver.com>
2023-06-29 16:20:08 +0900

    ㅇㅅㅇ

--- src/main/resources/static/js/admin/user/updateUser.js
+++ src/main/resources/static/js/admin/user/updateUser.js
@@ -1,6 +1,8 @@
+'use strict';
+let status;
 $(function() {
     let url = $(location).attr('href').split("/")[5];
-
+    let selectBox;
     let updateUser = new Vue({
         // el : 연결할 영역을 지정
         el: "#viewResult",
@@ -9,53 +11,75 @@ $(function() {
             user : {},
             statusList : {}
         },
-        method : {
-            checkStatus: function (status) {
-                for (let i in updateUser.statusList) {
-                    if (status == updateUser.statusList[i]) {
-                        alert(status);
-                        console.log($("#statusList"));
-                        $("#text").text(status)
-                        // $("#statusList").val(status).prop("selected", true);
-                        $(this).prop("selected", true);
-                        // alert(status);
-                        // $("#"+status+"").prop('selected', true);
-                    }
-                }
-            }
+        methods : {
+            checkStatus : function(state) {
+                alert("checkStatus 실행됨")
+                $("#statusList").trigger("click")
+            },
+            select: function(event) {
+                alert("select 실행됨")
+                const targetId = event.currentTarget.id;
+                selectBox = $("#"+targetId+"");
+
+                selectState(targetId);
+            },
+        },
+        mounted() {
+            alert("mount 실행");
+            const firstEl = $("#statusList");
+            console.log(firstEl)
+            // this.select();
+
+
+            selectBox.dispatchEvent(new Event("click"));
         }
     });
 
+    // function clickzz(object) {
+    //     console.log(object);
+    // }
+
     getStatus();
+
+    function clickzz(object) {
+        console.log(object);
+    }
+
+    function selectState(targetId) {
+        alert("selectState 실행:::::::::::::" + targetId)
+        $("#"+targetId+"").val(status).prop("selected", true);
+
+        const selectBox = $("#"+targetId+"");
+
+        console.log(selectBox);
+    }
 
     function searchUsers() {
         axios({
             url: '/am/user/'+url, // 통신할 웹문서
             method: 'get' // 통신 방식
         }).then(function (response) {
-            console.dir(response.data);
+            // console.dir(response.data);
             updateUser.user = response.data;
 
-            // let status = updateUser.user.status;
-            // checkStatus(status, updateUser);
+            status = updateUser.user.status;
         });
     }
-
-
+    
     function getStatus() {
         axios({
             url: '/am/getStatusList', // 통신할 웹문서
             method: 'get' // 통신 방식
         }).then(function (response) {
-            console.dir(response.data);
+            // console.dir(response.data);
             updateUser.statusList = response.data;
 
             searchUsers();
         });
     }
-
-
-
+    
+    
+    /* Button 영역 시작*/
     $("input#close").on("click", function() {
         window.open('','_self').close();
     })
@@ -63,17 +87,8 @@ $(function() {
     $("input#update").on("click", function() {
         location.href="/am/searchUser/";
     })
+    /* Button 영역 끝*/
+
+
 })
 
-// function checkStatus(status, updateUser) {
-//     for (let i in updateUser.statusList){
-//         if (status == updateUser.statusList[i]) {
-//             alert(status);
-//             console.log($("#statusList"));
-//             $("#text").val(status)
-//             $("#statusList").val(status).prop("selected", true);
-//             // alert(status);
-//             // $("#"+status+"").prop('selected', true);
-//         }
-//     }
-// }
